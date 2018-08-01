f905c9c4ecc451717c0042d93a0a70ecc05ca77e
- Edwin Mendez - <emendez@unac.edu.co>
2018-08-01 09:11:32 -0500

    metodo sumar - Operations class

--- Calculadora/src/calculadora/Operations.java
+++ Calculadora/src/calculadora/Operations.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package calculadora;
 
-/**
- *
- * @author Frank Bustamante
- */
+
 public class Operations {
+    
+    public double suma(double numero_1,double numero_2){
+        return numero_1 + numero_2;
+    }
     
 }

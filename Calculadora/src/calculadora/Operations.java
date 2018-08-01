8afbc435f49c3bec3b0c53351ce6b9b41a9598e0
AMiggel <antonnio762@gmail.com>
2018-08-01 16:39:56 -0500

    metodo dividir

--- Calculadora/src/calculadora/Operations.java
+++ Calculadora/src/calculadora/Operations.java
@@ -7,5 +7,8 @@ public class Operations {
     public double suma(double numero_1,double numero_2){
         return numero_1 + numero_2;
     }
+    public double dividir(double numero_1,double numero_2){
+        return numero_1 / numero_2;
+    }
     
 }
